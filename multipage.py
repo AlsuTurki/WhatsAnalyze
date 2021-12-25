d57052e894cca455a4f5e5b836a9bc66f83f3c98
AlsuTurki <turki.alsughayyir@gmail.com>
2021-12-25 06:15:23 +0300

    why are you looking at my commits?

--- multipage.py
+++ multipage.py
@@ -41,9 +41,9 @@ class MultiPage:
         st.sidebar.info(
             "This an open source project and you are very welcome to **contribute** your awesome "
             "comments, questions, resources and apps as "
-            "[issues](https://github.com/AlsuTurki/Saudi-POS/issues) of or "
-            "[pull requests](https://github.com/AlsuTurki/Saudi-POS/pulls) "
-            "to the [source code](https://github.com/AlsuTurki/Saudi-POS). ")
+            "[issues](https://github.com/AlsuTurki/WhatsAnalyze/issues) of or "
+            "[pull requests](https://github.com/AlsuTurki/WhatsAnalyze/pulls) "
+            "to the [source code](https://github.com/AlsuTurki/WhatsAnalyze). ")
             
         st.sidebar.info(
             """
